9e0c94f99454bc51ba0b87832746f19e54da4788
James McMullan <mcmullan.jgh@gmail.com>
2021-06-10 15:17:41 +0100

    sucessful emojify test

--- notesSpec.js
+++ notesSpec.js
@@ -31,6 +31,41 @@ function testClearingLocalStorage() {
      else
         return false
 };
+
+function testClearingArray() {
+    itemsArray.push("This is our first note");
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    localStorage.clear();
+    itemsArray = [];
+    if(itemsArray.includes("This is our first note"))
+        return false
+    else
+        return true
+};
+function testAPI(){
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then(response => response.json())
+    .then(data => console.log(data))  
+}
+
+const stuff = {"text": "Hello, :earth_africa:"};
+function testpostAPI() {
+    fetch('https://makers-emojify.herokuapp.com/', {
+        method: 'POST', // or 'PUT'
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(stuff),
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+    }      
+
 // click clear 
 // expect page not to have content
 // our array is empty
